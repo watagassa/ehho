42bf7296e300b6457ccaf80ac95bf756c7999abf
watagassa <135955629+watagassa@users.noreply.github.com>
2025-03-18 20:20:35 +0900

    add:適当に動くよう作成

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'demo', home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[200],
+      appBar: AppBar(title: Text('Scaffold')),
+      body: Center(child: Text('Hello Scaffold')),
+    );
+  }
+}
