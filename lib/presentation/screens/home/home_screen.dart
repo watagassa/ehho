734749706ced785d0e058844a352611a2d93758e
watagassa <135955629+watagassa@users.noreply.github.com>
2025-03-20 15:32:50 +0900

    add: ホーム画面を実装し、ボトムナビゲーションバーでアクティビティ画面と履歴画面への遷移を追加

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:ehho/presentation/widgets/activity/activity_detail_screen.dart';
+import 'package:ehho/presentation/widgets/history/history_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// プロバイダー
+final indexProvider = StateProvider((ref) {
+  // 変化するデータ 0~1
+  return 0; // 0 からスタート
+});
+
+// 画面全体
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // インデックス
+    final index = ref.watch(indexProvider); //refがプロバイダーの鍵
+
+    // アイテムたち
+    const items = [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.directions_run),
+        label: 'アクティビティ',
+      ),
+
+      BottomNavigationBarItem(icon: Icon(Icons.history), label: '計測履歴'),
+    ];
+
+    // 下のバー
+    final bar = BottomNavigationBar(
+      items: items, // アイテムたち
+      backgroundColor: Colors.lightGreen, // バーの色
+      selectedItemColor: Colors.white, // 選ばれたアイテムの色
+      unselectedItemColor: Colors.black, // 選ばれていないアイテムの色
+      currentIndex: index, // インデックス
+      onTap: (index) {
+        // タップされたとき インデックスを変更する
+        ref.read(indexProvider.notifier).state = index;
+      },
+    );
+
+    // 画面たち
+    final pages = [const ActivityScreen(), const HistoryScreen()];
+
+    return Scaffold(body: pages[index], bottomNavigationBar: bar);
+  }
+}
