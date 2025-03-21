d44fe1a5fb2a91c64882de736129e438003492f5
watagassa <135955629+watagassa@users.noreply.github.com>
2025-03-21 23:30:14 +0900

    add: 新規登録画面を実装

---
+++ lib/presentation/screens/sign_up/sign_up_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+// import 'signup_view_model.dart';
+
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  // final _viewModel = SignUpViewModel();
+  bool isChecked = false; // valueプロパティに渡す変数
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('新規登録')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // TextField(
+            //   // controller: _viewModel.emailController,
+            //   decoration: InputDecoration(labelText: 'メールアドレス'),
+            // ),
+            // TextField(
+            //   // controller: _viewModel.passwordController,
+            //   decoration: InputDecoration(labelText: 'パスワード'),
+            //   obscureText: true,
+            // ),
+            Text("aaa"),
+            TextField(
+              // controller: _viewModel.nameController,
+              maxLength: 20,
+              decoration: InputDecoration(labelText: '名前'),
+            ),
+            Text("aaa"),
+            TextField(
+              // controller: _viewModel.heightController,
+              decoration: InputDecoration(labelText: '身長（cm）'),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.allow(RegExp(r'^\d+(\.\d*)?')),
+              ],
+            ),
+            TextField(
+              // controller: _viewModel.weightController,
+              decoration: InputDecoration(labelText: '体重（kg）'),
+              keyboardType: TextInputType.number,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.allow(RegExp(r'^\d+(\.\d*)?')),
+              ],
+            ),
+            // gpsの許可チェックボックス
+            Row(
+              children: [
+                Checkbox(
+                  value: isChecked,
+                  onChanged: (value) {
+                    setState(() {
+                      isChecked = value!; // チェックボックスに渡す値を更新する
+                    });
+                  },
+                ),
+                Text("gpsの許可"),
+              ],
+            ),
+
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                // final success = await _viewModel.signUp();
+                // if (success) {
+                //   ScaffoldMessenger.of(context).showSnackBar(
+                //     SnackBar(content: Text('登録成功！')),
+                //   );
+                //   // TODO: アクティビティ画面へ遷移
+                // } else {
+                //   ScaffoldMessenger.of(context).showSnackBar(
+                //     SnackBar(content: Text('登録失敗')),
+                //   );
+                // }
+              },
+              child: Text('登録'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
