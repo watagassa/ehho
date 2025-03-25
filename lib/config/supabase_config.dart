f8dc87d5af6fab32d1d35c3318e27b68a57681d6
ranbudayo <45906970+ranbudayo@users.noreply.github.com>
2025-03-25 17:22:20 +0900

    add: ログイン処理完成

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -3,11 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 // Supabaseクライアントのプロバイダー
+// 任意のファイルでsupabaseを呼び出したい時はこれを使う
 final supabaseClientProvider = Provider<SupabaseClient>((ref) {
   return Supabase.instance.client;
 });
 
 // Supabaseの初期化
+// main関数で初期化する用
 Future<void> initSupabase() async {
   final supabaseUrl = dotenv.env['PUBLIC_SUPABASE_URL'];
   final supabaseAnonKey = dotenv.env['PUBLIC_SUPABASE_ANON_KEY'];
