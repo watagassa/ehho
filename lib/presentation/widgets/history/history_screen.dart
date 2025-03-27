d411877f2dc1ea19a659948450f473c5062909b1
watagassa <135955629+watagassa@users.noreply.github.com>
2025-03-27 17:58:18 +0900

    履歴画面に運動量バーを追加し、アクティビティ履歴の表示を改善

--- lib/presentation/widgets/history/history_screen.dart
+++ lib/presentation/widgets/history/history_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ehho/presentation/widgets/history/activity_history_list.dart';
+import 'package:ehho/presentation/widgets/history/mileage_bar.dart';
 import 'package:flutter/material.dart';
 // import 'package:go_router/go_router.dart';
 
@@ -10,7 +11,21 @@ class HistoryScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(children: [AppBar(), ActivityHistoryList()]),
+        child: Column(
+          children: [
+            AppBar(
+              leading: Icon(Icons.history), // 左側のアイコン
+              title: Text("アクティビティ履歴"),
+            ),
+            ActivityHistoryList(),
+            SizedBox(height: 20),
+            AppBar(
+              leading: Icon(Icons.trending_up), // 左側のアイコン
+              title: Text("運動量"),
+            ),
+            MileageBar(mileage: 5, maxMileage: 10),
+          ],
+        ),
       ),
     );
   }
