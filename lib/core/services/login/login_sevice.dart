6226c5e69b59ccb0d227e88e8429deda0c6b9117
watagassa <135955629+watagassa@users.noreply.github.com>
2025-03-27 22:23:42 +0900

    Revert "ログイン機能が動かなかったので、もとに戻す"
    This reverts commit 1019bc8ac5d623dfea0090664db5f722213c7d99.

---
+++ lib/core/services/login/login_sevice.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:ehho/config/supabase_config.dart';
+
+// LoginSevice プロバイダー
+final loginServiceProvider = Provider<LoginService>((ref) {
+  return LoginService(ref);
+});
+
+class LoginService {
+  final Ref _ref;
+  // 最新のsupabaseクライアントを取得する
+  SupabaseClient get _supabaseClient => _ref.read(supabaseClientProvider);
+  
+  LoginService(this._ref); // コンストラクタ
+
+  // Android, IOS 用
+  Future<void> googleLogin() async {
+    final webClientId = dotenv.env['WEB_CLIENT_ID'];
+    final androidClientId = dotenv.env['ANDROID_CLIENT_ID'];
+    final iosClientId = dotenv.env['IOS_CLIENT_ID'];
+
+    if (webClientId == null || androidClientId == null || iosClientId == null) {
+      throw Exception('envファイルにweb,android,ios関連の値が見つかりませんでした。');
+    }
+
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      clientId: Platform.isAndroid ? androidClientId : iosClientId,
+      serverClientId: webClientId,
+    );
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser!.authentication;
+    final accessToken = googleAuth.accessToken;
+    final idToken = googleAuth.idToken;
+
+    if (accessToken == null) {
+      throw 'アクセストークンが見つかりませんでした。';
+    }
+    if (idToken == null) {
+      throw 'IDトークンが見つかりませんでした。';
+    }
+
+    await _supabaseClient.auth.signInWithIdToken(
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
+  }
+}
+
