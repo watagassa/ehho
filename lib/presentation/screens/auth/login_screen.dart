71565bd214bdc97f1c9b9905b94c00dbbf3c04fc
x23062 <x23062xx@aitech.ac.jp>
2025-03-28 17:05:07 +0900

    ログイン画面のデザイン

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -10,48 +10,72 @@ class LoginScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // アプリ名
-              const Text(
-                "ehho",
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 40),
+        padding: const EdgeInsets. all(0),
+        child: Column(
+          children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background_login.png',
+              fit: BoxFit.cover,
+              // fit: BoxFit.fitWidth,
+            ),
+          ),
+          
+          const SizedBox(height: 20),
 
-              // ログインボタン
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    final loginService = ref.read(loginServiceProvider);
-                    try {
-                      await loginService.googleLogin();
-                    } catch (e) {
-                      throw ('Googleログインに失敗しました: $e');
-                    }
-                    if (context.mounted) {
-                      // ログイン処理が終わり次第signInに遷移
-                      context.go("/signIn");
-                    }
-                  },
-                  child: const Text("Googleでログイン"),
+          //テキスト
+          Align(
+            alignment: Alignment.center,
+            child: Column( 
+              mainAxisSize: MainAxisSize.min,
+              children: [ 
+                Text("ｴｯﾎｴｯﾎ", style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),),
+                Text("ここからログインできるって伝えなきゃ", style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),),
+                Text("ｴｯﾎｴｯﾎ", style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),),  
+              ],
+            ),
+          ),
+          
+          const SizedBox(height: 20),
+
+          // ログインボタン
+          SizedBox(
+            width: 300,
+              child: ElevatedButton(
+                onPressed: () async {
+                  final loginService = ref.read(loginServiceProvider);
+                  try {
+                    await loginService.googleLogin();
+                  } catch (e) {
+                    throw ('Googleログインに失敗しました: $e');
+                  }
+                  if (context.mounted) {
+                    // ログイン処理が終わり次第signInに遷移
+                    context.go("/signIn");
+                  }
+                },
+
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black54,
+                  padding: const EdgeInsets.all(5.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8), 
+                  ),
+                ),
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/google_icon.png', width: 32, height: 32),
+                    const SizedBox(width: 10),
+                    Text("Googleでログイン",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  ],
                 ),
               ),
-              const SizedBox(height: 20),
-
-              // 新規登録ボタン
-              TextButton(
-                onPressed: () {
-                  // 新規登録画面へ遷移
-                },
-                child: const Text("新規登録はこちら"),
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 20),
+          ],
         ),
       ),
     );
