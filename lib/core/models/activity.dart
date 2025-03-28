dd53e3498ffcb10e1d262c3d03041202c1c27911
ranbudayo <45906970+ranbudayo@users.noreply.github.com>
2025-03-28 22:25:43 +0900

    update: クラスの関数名を変更 既存の物と重複していたため

--- lib/core/models/activity.dart
+++ lib/core/models/activity.dart
@@ -27,7 +27,7 @@ class ActivitySend {
 
 
 /// supabaseから受信したデータを管理するクラス
-class Activity {
+class ActivityGet {
   int activity_id;
   String user_id;
   int activity;
@@ -36,7 +36,7 @@ class Activity {
   String day;
 
   /// 配列用の初期化関数があるからこれは使わないで！！！！
-  Activity(Map<String, dynamic> data)
+  ActivityGet(Map<String, dynamic> data)
     : activity_id = data['activity_id'] as int,
       user_id = data['user_id'] as String,
       activity = data['activity'] as int,
@@ -50,9 +50,9 @@ class Activity {
   /// supabaseで取得した配列を、このクラスとしてそれぞれ初期化する。
   /// 
   /// コンストラクタは使わずこちらで初期化してください。
-  static List<Activity> listInit(List<Map<String, dynamic>> listData) {
+  static List<ActivityGet> listInit(List<Map<String, dynamic>> listData) {
     return listData
-        .map((data) => Activity(data))
+        .map((data) => ActivityGet(data))
         .toList();
   }
 
