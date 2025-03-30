f1df01e901f4530395e3d64df0f9d943ff1d3bd6
skyrocket05 <the_sky.777@icloud.com>
2025-03-30 12:54:58 +0900

    GPSの許可

--- lib/core/services/map/googlemap_service.dart
+++ lib/core/services/map/googlemap_service.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 // Google Mapsのパッケージをインポートする
 import 'package:google_maps_flutter/google_maps_flutter.dart'; // 追加
-
+// Geolocatorのパッケージをインポートする
+import 'package:geolocator/geolocator.dart';
 
 
 class MapView extends StatefulWidget {
@@ -11,10 +12,46 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-// マップビューの初期位置
-  CameraPosition _initialLocation = CameraPosition(target: LatLng(0.0, 0.0)); // 追加
+    // マップビューの初期位置
+  CameraPosition _initialLocation = CameraPosition(target: LatLng(0.0, 0.0));
   // マップの表示制御用
-  late GoogleMapController mapController; // 追加
+  late GoogleMapController mapController;
+
+  // 現在位置の記憶用
+  late Position _currentPosition; // 追加
+
+    // 現在位置の取得方法
+  _getCurrentLocation() async {
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) async {
+      setState(() {
+        // 位置を変数に格納する
+        _currentPosition = position;
+
+        print('CURRENT POS: $_currentPosition');
+
+          // カメラを現在位置に移動させる場合
+        mapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(
+                position.latitude, position.longitude),
+              zoom: 18.0,
+            ),
+          ),
+        );
+      });
+    // await _getAddress();
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  @override
+    void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +77,95 @@ class _MapViewState extends State<MapView> {
               onMapCreated: (GoogleMapController controller) {
                 mapController = controller;
               },
+            ),
+            // ここからボタンを表示するためのコードを追加
+            // ズームイン・ズームアウトのボタンを配置
+            SafeArea(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10.0, bottom: 100.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      // ズームインボタン
+                      ClipOval(
+                        child: Material(
+                          color: Colors.blue.shade100, // ボタンを押す前のカラー
+                          child: InkWell(
+                            splashColor: Colors.blue, // ボタンを押した後のカラー
+                            child: SizedBox(
+                              width: 40,
+                              height: 30,
+                              child: Icon(Icons.add),
+                            ),
+                            onTap: () {
+                              mapController.animateCamera(
+                                CameraUpdate.zoomIn(),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 35),
+                      //　ズームアウトボタン
+                      ClipOval(
+                        child: Material(
+                          color: Colors.blue.shade100, // ボタンを押す前のカラー
+                          child: InkWell(
+                            splashColor: Colors.blue, // ボタンを押した後のカラー
+                            child: SizedBox(
+                              width: 40,
+                              height: 30,
+                              child: Icon(Icons.remove),
+                            ),
+                            onTap: () {
+                              mapController.animateCamera(
+                                CameraUpdate.zoomOut(),
+                              );
+                            },
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SafeArea(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10.0, bottom: 10.0),
+                  // 現在地表示ボタン
+                  child: ClipOval(
+                    child: Material(
+                      color: Colors.orange.shade100, // ボタンを押す前のカラー
+                      child: InkWell(
+                        splashColor: Colors.blue, // ボタンを押した後のカラー
+                        child: SizedBox(
+                          width: 40,
+                          height: 30,
+                          child: Icon(Icons.my_location),
+                        ),
+                        onTap: () {
+                          mapController.animateCamera(
+                            CameraUpdate.newCameraPosition(
+                              CameraPosition(
+                                target: LatLng(
+                                    _currentPosition.latitude,
+                                    _currentPosition.longitude,
+                                ),
+                                zoom: 18.0,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
