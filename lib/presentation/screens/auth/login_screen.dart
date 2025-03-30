8a45a7f23c00037c34b0d449a99d17638e22a2c9
ryota <mc_ryouta3@yahoo.co.jp>
2025-03-30 13:34:09 +0900

    update: コード修正

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -11,37 +11,55 @@ class LoginScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets. all(0),
+        padding: const EdgeInsets.all(0),
         child: Column(
           children: [
-          Positioned.fill(
-            child: Image.asset(
-              'assets/images/background_login.png',
-              fit: BoxFit.cover,
-              // fit: BoxFit.fitWidth,
+            Positioned.fill(
+              child: Image.asset(
+                'assets/images/background_login.png',
+                fit: BoxFit.cover,
+                // fit: BoxFit.fitWidth,
+              ),
             ),
-          ),
-          
-          const SizedBox(height: 20),
 
-          //テキスト
-          Align(
-            alignment: Alignment.center,
-            child: Column( 
-              mainAxisSize: MainAxisSize.min,
-              children: [ 
-                Text("ｴｯﾎｴｯﾎ", style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),),
-                Text("ここからログインできるって伝えなきゃ", style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),),
-                Text("ｴｯﾎｴｯﾎ", style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),),  
-              ],
+            const SizedBox(height: 20),
+
+            //テキスト
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "ｴｯﾎｴｯﾎ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    "ここからログインできるって伝えなきゃ",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Text(
+                    "ｴｯﾎｴｯﾎ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          
-          const SizedBox(height: 20),
 
-          // ログインボタン
-          SizedBox(
-            width: 300,
+            const SizedBox(height: 20),
+
+            // ログインボタン
+            SizedBox(
+              width: 300,
               child: ElevatedButton(
                 onPressed: () async {
                   try {
@@ -49,11 +67,15 @@ class LoginScreen extends ConsumerWidget {
                   } catch (e) {
                     throw ('Googleログインに失敗しました: $e');
                   }
+                  final isRegistered =
+                      await ref.read(authServiceProvider).isRegistered();
                   if (context.mounted) {
-                    final isRegistered = await ref.read(authServiceProvider).isRegistered();
-                    // if(!isRegistered) context.go("/signIn");
-                    // else context.go("/home");
-                    context.go("/signIn");
+                    if (!isRegistered) {
+                      context.go("/signIn");
+                    } else {
+                      context.go("/home");
+                    }
+                    // context.go("/signIn");
                   }
                 },
 
@@ -62,16 +84,26 @@ class LoginScreen extends ConsumerWidget {
                   foregroundColor: Colors.black54,
                   padding: const EdgeInsets.all(5.0),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8), 
+                    borderRadius: BorderRadius.circular(8),
                   ),
                 ),
 
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset('assets/images/google_icon.png', width: 32, height: 32),
+                    Image.asset(
+                      'assets/images/google_icon.png',
+                      width: 32,
+                      height: 32,
+                    ),
                     const SizedBox(width: 10),
-                    Text("Googleでログイン",style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    Text(
+                      "Googleでログイン",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
               ),
