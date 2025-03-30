d1504d07e16e9f364259c1ddcaeb41b7476cbb25
watagassa <135955629+watagassa@users.noreply.github.com>
2025-03-30 15:29:59 +0900

    update: GPS許可確認ボタンの実装とサインアップ画面への統合

--- lib/presentation/widgets/gpsAllow_button.dart
+++ lib/presentation/widgets/gpsAllow_button.dart
@@ -1,82 +1,46 @@
+import 'package:ehho/core/services/map/gps_allow.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 
-class LocationPage extends StatefulWidget {
+class GpsallowButton extends StatefulWidget {
   @override
-  _LocationPageState createState() => _LocationPageState();
+  _GpsallowButtonState createState() => _GpsallowButtonState();
 }
 
-class _LocationPageState extends State<LocationPage> {
-  String _currentLocation = '現在地を取得中...';
-
-  // _determinePosition関数の実行
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // 位置情報サービスが有効かどうかをテストします。
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // 位置情報サービスが有効でない場合、続行できません。
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      // ユーザーに位置情報を許可してもらうよう促す
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // 拒否された場合エラーを返す
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    // 永久に拒否されている場合のエラーを返す
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // 位置情報に対しての権限が許可されている場合、デバイスの位置情報を返す
-    return await Geolocator.getCurrentPosition();
-  }
-
-  // ボタンを押したときに現在地を取得
-  void _getCurrentLocation() async {
-    try {
-      Position position = await _determinePosition();
-      setState(() {
-        _currentLocation =
-            '緯度: ${position.latitude}, 経度: ${position.longitude}';
-      });
-    } catch (e) {
-      setState(() {
-        _currentLocation = e.toString();
-      });
-    }
-  }
+class _GpsallowButtonState extends State<GpsallowButton> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('現在地取得')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              _currentLocation,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _getCurrentLocation,
-              child: Text('現在地を取得'),
-            ),
-          ],
+    return Row(
+      children: [
+        Checkbox(
+          value: isChecked,
+          onChanged: (value) async {
+            bool result = false;
+            // チェックボックスがチェックされた場合、位置情報の許可を確認
+            if (!isChecked) {
+              try {
+                result = await gpsAllow();
+                if (result) {
+                  // 許可された場合の処理
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("位置情報の許可が得られました")));
+                }
+              } catch (e) {
+                // 許可されなかった場合の処理
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text("位置情報の許可に失敗しました: $e")));
+              }
+              setState(() {
+                isChecked = result;
+              });
+            }
+          },
         ),
-      ),
+        Text("gpsの許可"),
+      ],
     );
   }
 }
